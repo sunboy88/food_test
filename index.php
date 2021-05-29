597729fcddd7834538c46cd62c62da079650ed2b
Jack Nguyen <easier.web@gmail.com>
2021-05-29 18:48:01 +0700

    commit first 1

--- index.php
+++ index.php
@@ -43,7 +43,7 @@
  *
  * You can load different configurations depending on your
  * current environment. Setting the environment also influences
- * things like logging and error reporting.
+ * things like logging and error reporting.dsasdsad
  *
  * This can be set to anything, but default usage is:
  *
