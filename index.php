888c9fa691d9328abdd5617db4581ff4592d2b41
Jack Nguyen <easier.web@gmail.com>
2021-05-29 18:49:31 +0700

    commit first 1

--- index.php
+++ index.php
@@ -262,7 +262,7 @@ dsadsad
 	{
 		header('HTTP/1.1 503 Service Unavailable.', TRUE, 503);
 		echo 'Your application folder path does not appear to be set correctly. Please open the following file and correct this: '.SELF;
-		exit(3); // EXIT_CONFIG
+		exit(3); // EXIT_CONFIGdsadasdsad
 	}
 
 	define('APPPATH', $application_folder.DIRECTORY_SEPARATOR);
