e0537177e3c1b6b2aa2310607eb68c11a3ccb3b6
Jack Nguyen <easier.web@gmail.com>
2021-05-29 18:49:47 +0700

    commit first 1

--- index.php
+++ index.php
@@ -137,7 +137,7 @@ dssdsd
  * DEFAULT CONTROLLER
  * --------------------------------------------------------------------
  *
- * Normally you will set your default controlledssdr in the routes.php file.
+ * Normally you will set your default condsadsasdtrolledssdr in the routes.php file.
  * You can, however, force a custom routing by hard-coding a
  * specific controller class/function here. For most applications, you
  * WILL NOT set your routing here, but it's adsdasdsn odsdsdption for those
