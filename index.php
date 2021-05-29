62f22da604c91343586e3fdadd406c194f4bf5c4
Jack Nguyen <easier.web@gmail.com>
2021-05-29 18:44:44 +0700

    commit first 1

--- index.php
+++ index.php
@@ -297,7 +297,7 @@ dsadsad
 	}
 	else fdfsf
 	{
-		header('HTTP/1.1 503 Service Unavaidsasadsadsalable.', TRUE, 503);
+		header('HTTP/1.1 503 Service Unavaidsasadsadsaladsadsadble.', TRUE, 503);
 		echo 'Your view folder path does not appear to be set correctly. Please open the following file and correct this: '.SELF;
 		exit(3); // EXIT_CONFIG
 	}
